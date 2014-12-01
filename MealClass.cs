7e3d313f59f5c7f49c393b42bc5b2f69ebcf7429
Kelsey <kma3a@virginia.edu>
2014-12-01 16:26:26 -0600

    added initto meal class

--- MealClass.cs
+++ MealClass.cs
@@ -1,11 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 namespace Menu
 {
 	public class MealClass
 	{
-		public MealClass ()
+		string entree;
+		string side;
+		string drink;
+		string dessert;
+
+		MealClass()
 		{
+			entree = "error";
+			side = "error";
+			drink = "error";
+			dessert = "error";
+		}
+		public MealClass(Dictionary<string, string> args)
+		{
+			entree = args ["entree"];
+			side = args["side"];
+			drink = args["drink"];
+			dessert = args["dessert"];
 		}
 	}
 }
--- MealTestClass.cs
+++ MealTestClass.cs
@@ -1,11 +1,28 @@
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace Menu
 {
 	[TestFixture()]
 	public class MealTestClass
 	{
+		MealClass testMealMorning;
+		MealClass testMealNight;
+		[SetUp]public void Init()
+		{
+			//	Dictionary<string, string> morning = new Dictionary<string, string> ();
+//			morning.Add ("entree", "eggs");
+//			morning.Add ("side", "toast");
+//			morning.Add ("drink", "coffee");
+			Dictionary<string, string> night = new Dictionary<string, string> ();
+			night.Add ("entree", "steak");
+			night.Add ("side", "potato");
+			night.Add ("drink", "wine");
+			night.Add ("dessert", "cake");
+//			testMealMorning = new MealClass (morning);
+			testMealNight = new MealClass (night);
+		}
 		[Test()]
 		public void TestCase ()
 		{
